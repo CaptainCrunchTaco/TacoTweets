925b6c095445289febccbdd551ecc2c90bf85fcf
Ariel Ledesma <ariel.led023@gmail.com>
2015-03-08 22:07:18 -0700

    Created new icon

--- app/src/main/java/com/codepath/apps/TacoTweets/activity/LoginActivity.java
+++ app/src/main/java/com/codepath/apps/TacoTweets/activity/LoginActivity.java
@@ -17,6 +17,7 @@ public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+        getSupportActionBar().setElevation(0);
 	}
 
 
